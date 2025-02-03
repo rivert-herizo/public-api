c55be627e056b47908ad9f50506c5a0bbcfa13c2
Hello <totoroherizo@gmail.com>
2025-02-03 09:42:40 +0300

    third commit

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import axios from 'axios';
 
 const app = express();
 const PORT = 3000;
+
+const api_url = "https://rickandmortyapi.com/api/character/1";
+
+
 
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.render("index.ejs");
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(api_url);
+        console.log(response.data.name);
+        res.render('index.ejs', { character: response.data });
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
 });
 
 app.listen(PORT, () => {
